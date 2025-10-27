b224a83937577bb19974a5427d2cfd613820e0b8
kuriyamashuto <kuri.shu117@gmail.com>
2025-10-27 21:59:24 +0900

    再提出①

---
+++ accordion-anime/work/js/app.js
@@ -0,0 +1,2 @@
+console.log("main.js 読み込まれた！");
+const accordions = document.querySelectorAll('.list-item'); accordions.forEach(accordion => { accordion.addEventListener('click', () => { const content = accordion.querySelector('.list-content'); content.classList.toggle('active'); if(content.classList.contains('active')) { content.style.height = content.scrollHeight + 'px'; } else { content.style.height = '0px'; } }); });
